b8f22c8b43a733e44bfe5c979cf46a4065fe6d60
Marlon Hildon Junior <marlonhildon@gmail.com>
2019-02-21 10:35:51 -0300

    Inserção de usuário com POST

--- src/main/java/com/marlonhildon/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marlonhildon/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.marlonhildon.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marlonhildon.workshopmongo.domain.User;
 import com.marlonhildon.workshopmongo.dto.UserDTO;
@@ -36,5 +39,13 @@ public class UserResource {
 		User obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@RequestMapping(method = RequestMethod.POST) // @PostMapping também funciona
+	public ResponseEntity<Void> findById(@RequestBody UserDTO objDTO) {	//A anotação serve para o objeto ser aceito pelo endpoint.
+		User obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();	//Pega o endereço do novo objeto criado
+		return ResponseEntity.created(uri).build();		// O created retorna o código 201 com o cabeçalho contendo a localização do novo recurso criado
 	}
 }
