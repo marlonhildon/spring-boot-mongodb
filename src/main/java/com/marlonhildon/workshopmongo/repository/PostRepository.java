83be014b9c04c148eb655cfa8c57fd3aacc5bba5
Marlon Hildon Junior <marlonhildon@gmail.com>
2019-02-22 11:11:34 -0300

    Consulta simples com @Query

--- src/main/java/com/marlonhildon/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marlonhildon/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.marlonhildon.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marlonhildon.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.marlonhildon.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")	//Consulta simples com @Query e $regex
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text); // O Spring monta a consulta apenas digitando esse query method.
 }
